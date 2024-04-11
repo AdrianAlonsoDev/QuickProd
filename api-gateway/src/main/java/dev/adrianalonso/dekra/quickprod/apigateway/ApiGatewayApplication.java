d77b39e529fbd8370ae99c478203ac18eeb7f265
Adrián Alonso <adrianartimez@outlook.com>
2024-04-11 08:23:01 +0200

    fixed config server for customer, replicate 4 others

--- api-gateway/src/main/java/dev/adrianalonso/dekra/quickprod/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/dev/adrianalonso/dekra/quickprod/apigateway/ApiGatewayApplication.java
@@ -1,11 +1,48 @@
 package dev.adrianalonso.dekra.quickprod.apigateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Ruta para Customer Services
+                .route("customer-services", r -> r.path("/api/v1/customers/**")
+                        .uri("lb://CUSTOMER-SERVICES"))
+
+                // Ruta para Fraud Services
+                .route("fraud-services", r -> r.path("/api/v1/fraud-checks/**")
+                        .uri("lb://FRAUD-SERVICES"))
+
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("QP | Customer Service")
+                        .description("Customer Service API de QuickProd")
+                        .version("1.0").contact(new Contact()
+                                .name("Adrian Alonso")
+                                .email("adrianalonsodev@outlook.com")));
     }
 }
